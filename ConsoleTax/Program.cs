fe23186b596d307e4a0808f39229efabb535a3aa
Zain ul abdien <74106328+Surge69420@users.noreply.github.com>
2025-03-18 11:25:49 +0000

    Add project files.

---
+++ ConsoleTax/Program.cs
@@ -0,0 +1,12 @@
+﻿using Data;
+
+namespace ConsoleTax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
