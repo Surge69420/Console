6c4ec6fa2b6524ff6dc3762fbb97d72d217078c9
Zain ul abdien <74106328+Surge69420@users.noreply.github.com>
2025-03-18 11:37:15 +0000

    Remove Class1 and add Entity Framework Core support
    - Deleted `Class1.cs`.
    - Updated `Data.csproj` to include Entity Framework Core packages.
    - Introduced `serverDbContext` class in `DbContext.cs` for managing `TaxPayer` entities.
    - Added `TaxPayers.cs` to define the `TaxPayer` model.
    - Created `DbService.cs` for potential database service layer.

--- Data/Data/DbContext.cs
+++ Data/Data/DbContext.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data.Models;
+using Data.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data.Data
 {
-    class DbContext
+    class serverDbContext : DbContext
     {
+        public DbSet<TaxPayer> TaxPayers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer($"data source=DESKTOP-7R8OIVU\\SQLEXPRESS01;initial catalog=TaxPayers;Trusted_Connection=True;TrustServerCertificate=True");
+        }
     }
 }
+
